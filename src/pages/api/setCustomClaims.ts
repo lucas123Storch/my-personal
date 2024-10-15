8302d88cc8824daafbc68f897e84e3c8f98a487f
Lucas Storch <lucasstorch.ls@gmail.com>
2024-10-15 08:43:30 -0300

    error

--- src/pages/api/setCustomClaims.ts
+++ src/pages/api/setCustomClaims.ts
@@ -6,11 +6,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { uid, role } = req.body;
 
     try {
-      // Definir a claim personalizada no usuário
       await admin.auth().setCustomUserClaims(uid, { [role]: true });
       res.status(200).json({ message: "Claims definidas com sucesso." });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        res.status(500).json({ error: error.message });
+      } else {
+        res.status(500).json({ error: "Erro desconhecido" });
+      }
     }
   } else {
     res.status(405).json({ message: "Método não permitido" });
